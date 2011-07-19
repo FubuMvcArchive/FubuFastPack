5d5cb0203a9afbb60afe153529c4fddebb10a233
Brandon Behrens <brandonbehrens@gmail.com>
2011-07-19 13:39:28 -0500

    Fixed a bunch of non terminated statements.  When we get the criteria, we're now returning an object instead of a jquery object.  Was just happenstance this worked to begin with.

--- src/FubuFastPack/WebContent/Content/Scripts/fubu.query.js
+++ src/FubuFastPack/WebContent/Content/Scripts/fubu.query.js
@@ -1,14 +1,14 @@
-﻿$.fn.asQueryBuilder = function (userOptions) {
-    var options = {};
-    $.extend(options, $.fn.asQueryBuilder.defaults, userOptions || {});
+﻿$.fn.asQueryBuilder = function(userOptions) {
+    var options = { };
+    $.extend(options, $.fn.asQueryBuilder.defaults, userOptions || { });
 
-    return this.each(function (i, div) {
+    return this.each(function(i, div) {
         div.tbody = $('tbody', div).get(0);
         div.grid = $('#' + $(div).metadata().filters.gridId).get(0);
 
-        var queryRunner = function () {
+        var queryRunner = function() {
             var notification = div.validate();
-            if (notification.errors.length > 0){
+            if (notification.errors.length > 0) {
                 $(div).trigger('invalid-filters', notification);
                 return;
             }
@@ -19,34 +19,34 @@
             div.grid.runQuery(criterion);
 
             return false;
-        }
+        };
 
-        div.getCriteria = function () {
-            return $('tr', div.tbody).map(function () { return this.getCriteria(); });
-        }
+        div.getCriteria = function() {
+            return $.map($('tr', div.tbody), function (row) { return row.getCriteria(); });
+        };
 
         div.validate = function() {
             var messages = [];
-			
-			$('tr', div.tbody).each(function(i, row){
-				var message = row.validate();
-				if (message == null || message.messages.length == 0) return;
-				
-				messages.push(message);
-			});
-			
-            return {errors: messages};
-        }
 
-        div.hasCriteria = function () {
+            $('tr', div.tbody).each(function(i, row) {
+                var message = row.validate();
+                if (message == null || message.messages.length == 0) return;
+
+                messages.push(message);
+            });
+
+            return { errors: messages };
+        };
+
+        div.hasCriteria = function() {
             return $('tr', div.tbody).length > 0;
-        }
+        };
 
         $(options.runQuerySelector).click(queryRunner);
 
         if (typeof this.context == 'undefined' || this.context == document) {
             // We cannot wire up the Enter key if a context was used in the selector
-            $("#" + div.id + " input[type=text]").live("keypress", function (keyEvent) {
+            $("#" + div.id + " input[type=text]").live("keypress", function(keyEvent) {
                 if (keyEvent.which == 13) {
                     queryRunner();
                 }
@@ -55,28 +55,28 @@
 
         var templates = $('.templates', div).get(0);
 
-        div.addFilterRow = function () {
+        div.addFilterRow = function() {
             var row = $('<tr class="filter"><td></td><td></td><td></td><td></td></tr>').appendTo(div.tbody).asFilterRow(templates, options, div);
             $(div).trigger("filter-added", row);
             $(div).trigger("filters-changed");
             return row;
-        }
+        };
 
-        $(options.addCriteriaSelector).click(function () {
+        $(options.addCriteriaSelector).click(function() {
             div.addFilterRow();
             return false;
         });
 
-        var clearFilters = function () {
+        var clearFilters = function() {
             $(div.tbody).empty();
             $(div).trigger('filters-cleared');
             $(div).trigger("filters-changed");
             return false;
-        }
+        };
 
         $(options.clearCriteriaSelector).click(clearFilters);
 
-        div.loadQuery = function (criterion) {
+        div.loadQuery = function(criterion) {
             if (criterion.length == 0) return;
 
             clearFilters();
@@ -85,7 +85,7 @@
                 var row = this.addFilterRow();
                 row.setCriteria(criterion[i]);
             }
-        }
+        };
 
         var metadata = $(div).metadata();
         if (metadata.filters.initialCriteria.length > 0) {
@@ -99,7 +99,7 @@
     });
 
 
-}
+};
 
 
 
@@ -117,7 +117,7 @@ function buildQueryOptions(onFormClear) {search-criteria-search
         clearCriteriaSelector: "#search-criteria-cancel",
         runQuery: runQuery,
         onFormClear: onFormClear
-    }
+    };
 }
 
 $(document).ready(function () {
@@ -125,75 +125,75 @@ $(document).ready(function () {
 });
 
 
-if( ! $.jgrid ){ 
-	$.jgrid = {
-		defaults: {
-			missingvalue: 'Missing value'
-		} 
-	}
+if( ! $.jgrid ){
+    $.jgrid = {
+        defaults: {
+            missingvalue: 'Missing value'
+        }
+    };
 }
 
 
 $.fn.asQueryBuilder.editors = {
     textbox: function (textbox) {
-        textbox.setValue = function (value) { $(textbox).val(value); }
-        textbox.getValue = function () { return $(textbox).val(); }
-        textbox.validate = function() {
+        textbox.setValue = function(value) { $(textbox).val(value); };
+        textbox.getValue = function () { return $(textbox).val(); };
+        textbox.validate = function () {
             var value = textbox.getValue();
-            if ($.trim(value).length == 0){
+            if ($.trim(value).length == 0) {
                 return [$.jgrid.defaults.missingvalue];
             }
 
             return [];
-        }
+        };
     }
 }
 
-$.fn.asFilterRow = function (templates, options, div) {
+$.fn.asFilterRow = function(templates, options, div) {
     var row = this.get(0);
     row.operatorCell = row.childNodes[1];
     row.editorCell = row.childNodes[2];
 
-    $(options.removeCriteriaTemplateSelector).clone().appendTo(row.childNodes[3]).click(function () {
+    $(options.removeCriteriaTemplateSelector).clone().appendTo(row.childNodes[3]).click(function() {
         $(row).remove();
         $(div).trigger("filter-removed");
         $(div).trigger("filters-changed");
         return false;
     });
 
-    row.findEditorTemplate = function () {
+    row.findEditorTemplate = function() {
         for (var i = 0; i < arguments.length; i++) {
             var finder = $(arguments[i], templates);
             if (finder.length > 0) return finder.clone(true);
         }
 
         return $('<input type="text"></input>');
-    }
+    };
 
-    row.changeProperty = function () {
+    row.changeProperty = function() {
         var propName = $(row.propertySelector).val();
         var selector = 'div.smart-grid-operators > select.' + propName;
 
         $(row.operatorCell).empty();
-        row.operatorSelector = $(selector, templates).clone(true).appendTo(row.operatorCell).change(function () {
+        row.operatorSelector = $(selector, templates).clone(true).appendTo(row.operatorCell).change(function() {
             row.changeOperator();
         });
 
         row.changeOperator();
 
         return false;
-    }
+    };
 
-    row.property = function () { return $(row.propertySelector).val(); }
-    row.operator = function () { return $(row.operatorSelector).val(); }
-    row.value = function () { return row.editor.getValue(); }
+    row.property = function() { return $(row.propertySelector).val(); };
+    row.operator = function() { return $(row.operatorSelector).val(); };
+    row.value = function() { return row.editor.getValue(); };
 
     row.propertySelector = $('select.smart-grid-properties', templates)
         .clone(true)
         .appendTo(row.childNodes[0])
         .change(row.changeProperty).get(0);
 
-    row.changeOperator = function () {
+    row.changeOperator = function() {
         $(row.editorCell).empty();
 
         // go find the editor
@@ -209,38 +209,38 @@ $.fn.asFilterRow = function (templates, options, div) {
 
         row.editor = template.get(0);
         $.fn.asQueryBuilder.editors[editorName](row.editor);
-    }
+    };
 
-    row.validate = function(){
-		if ($.isFunction(row.editor.validate) == false) return false;
-	
+    row.validate = function() {
+        if ($.isFunction(row.editor.validate) == false) return false;
+
         return {
             header: $('option:selected', row.propertySelector).text(),
             messages: row.editor.validate()
         };
-    }
+    };
 
-    row.getCriteria = function () {
+    row.getCriteria = function() {
         return {
             op: row.operator(),
             value: row.editor.getValue(),
             property: row.property()
-        }
+        };
     };
 
-    row.setCriteria = function (criteria) {
+    row.setCriteria = function(criteria) {
         $(row.propertySelector).val(criteria.property);
         row.changeProperty();
         row.operatorSelector.val(criteria.op);
         row.changeOperator();
         row.editor.setValue(criteria.value);
-    }
+    };
 
     row.changeProperty();
     row.changeOperator();
 
     return row;
-}
+};
 
 
 
