d738c7bc2e8597618772603b2caf7de648fe1f41
Brandon Behrens <brandonbehrens@gmail.com>
2011-09-12 14:33:12 -0500

    Checking in failing unit test

---
+++ src/FubuFastPack.Testing/NHibernate/ProjectionTester.cs
@@ -0,0 +1,27 @@
+﻿using FubuFastPack.NHibernate;
+using FubuTestApplication.Domain;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuFastPack.Testing.NHibernate
+{
+    [TestFixture]
+    public class when_adding_a_where_expression : InteractionContext<Projection<Case>>
+    {         
+        [Test]
+        public void the_where_should_be_added_to_the_aliases()
+        {
+            ClassUnderTest.Where(x => x.Person.Name).Equals("foo");
+            ClassUnderTest.Aliases.ShouldHaveCount(1);
+        }
+
+        [Test]
+        public void should_only_return_the_same_alias_one_time()
+        {
+            ClassUnderTest.Where(x => x.Person.Name).Equals("foo");
+            ClassUnderTest.Where(x => x.Person.Name).Equals("foo");
+            ClassUnderTest.Aliases.ShouldHaveCount(1);
+        }
+
+    }
+}
