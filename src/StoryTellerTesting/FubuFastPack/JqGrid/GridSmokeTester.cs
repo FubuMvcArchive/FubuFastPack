29c4371180badfb77f0fadfc75dff5aa6f8ff22c
Ryan Rauh <rauh.ryan@gmail.com>
2011-11-08 14:42:37 -0600

    fixed the smoke test

--- src/StoryTellerTesting/FubuFastPack/JqGrid/GridSmokeTester.cs
+++ src/StoryTellerTesting/FubuFastPack/JqGrid/GridSmokeTester.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using FubuFastPack.JqGrid;
+using FubuMVC.Core.Http;
 using FubuTestApplication;
 using FubuTestApplication.Grids;
 using NUnit.Framework;
@@ -14,12 +15,35 @@ namespace IntegrationTesting.FubuFastPack.JqGrid
         public void try_to_build_out_the_test_harness_case_grid()
         {
             var container = DatabaseDriver.GetFullFastPackContainer();
+            container.Configure(x => x.For<ICurrentHttpRequest>().Use<FakeCurrentHttpRequest>());
             container.ExecuteInTransaction<CaseController>(x =>
             {
                 Debug.WriteLine(x.AllCases().ToString());
             });
 
             
+        }
+    }
+    public class FakeCurrentHttpRequest : ICurrentHttpRequest
+    {
+        public string RawUrl()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string RelativeUrl()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ToFullUrl(string url)
+        {
+            return url;
+        }
+
+        public string HttpMethod()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
